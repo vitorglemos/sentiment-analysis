e404e0011ef19f4cc2b581943df4a54ab766749e
Vítor Lemos <31148456+vitorglemos@users.noreply.github.com>
2021-05-07 11:07:22 -0300

    Create model_v3

---
+++ model_v3.py
@@ -0,0 +1,47 @@
+num_features = 64
+num_labels = 7
+batch_size = 64
+epochs = 100
+width, height = 48, 48
+
+model = Sequential()
+
+model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', input_shape=(width, height, 1), data_format='channels_last', kernel_regularizer=l2(0.01)))
+model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+
+model.add(Dense(2*2*2*num_features, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(2*2*num_features, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(2*num_features, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(num_labels, activation='softmax'))
+
+model.summary()
